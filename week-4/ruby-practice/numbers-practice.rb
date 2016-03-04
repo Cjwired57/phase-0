08e08967b40c2f6ba930bdcb2fd6a56cdf86a621
Cjwired57 <cjlamkin64@gmail.com>
2016-03-04 08:50:48 -0800

    practice grade return

--- week-4/ruby-practice/numbers-practice.rb
+++ week-4/ruby-practice/numbers-practice.rb
@@ -1,4 +1,19 @@
-puts 'What is your favorite number?'
-favorite_number = gets.chomp
-better_number = favorite_number.to_i + 1
-puts 'Wait your favorite number is '+favorite_number.to_s+'? '+better_number.to_s+' is a way better number!'
+def get_grade(percentage)
+  if percentage >= 90 && percentage > 80
+    puts 'A'
+  end
+  if percentage >= 80 && percentage < 90
+    puts 'B'
+  end
+  if percentage >= 70 && percentage < 80
+    puts 'C'
+  end
+  if percentage >= 60 && percentage < 70
+    puts 'D'
+  end
+  if percentage >= 0 && percentage < 60
+    puts 'F'
+  end
+end
+
+get_grade(55)
