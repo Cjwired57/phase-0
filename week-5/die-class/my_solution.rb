dfb2ddc3f154478b38db7e1526df7dab9a8f6c99
Cjwired57 <cjlamkin64@gmail.com>
2016-03-14 09:05:53 -0700

    create die files

---
+++ week-5/die-class/my_solution.rb
@@ -0,0 +1,44 @@
+# Die Class 1: Numeric
+
+# I worked on this challenge [by myself, with: ]
+
+# I spent [] hours on this challenge.
+
+# 0. Pseudocode
+
+# Input:
+# Output:
+# Steps:
+
+
+# 1. Initial Solution
+
+class Die
+  def initialize(sides)
+    @sides = sides
+  end
+  def inspect
+    "This die has {@sides} sides."
+  end
+  def sides
+    sides = @sides
+    p sides
+  end
+
+  def roll
+    1 + rand(sides)
+  end
+
+end
+
+
+# 3. Refactored Solution
+
+
+
+
+
+
+
+# 4. Reflection
+
