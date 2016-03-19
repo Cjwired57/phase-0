632f9228909c5d775ec25aa28aaceeefd360d255
Cjwired57 <cjlamkin64@gmail.com>
2016-03-19 10:03:41 -0700

    add gps file

--- week-6/gps_original.rb
+++ week-6/gps_original.rb
@@ -1,24 +1,15 @@
 # Your Names
-# 1)Chris Lamkin
-# 2)Alec Hendrickson
-
-=begin
-1.) Refactor error counter using has_key?
-2.) Suggested baking items
-give user a more accurate message
-=end
+# 1)
+# 2)
 
 # We spent [#] hours on this challenge.
 
 # Bakery Serving Size portion calculator.
 
-#  Define method, creates hash, sets parameters for input
 def serving_size_calc(item_to_make, num_of_ingredients)
   library = {"cookie" => 1, "cake" =>  5, "pie" => 7}
-# Sets up an error counter at 3
+  error_counter = 3
 
-
-#Iterates over hash keys, if item_to_make does not equal hash key, it adds a negative error counter
   library.each do |food|
     if library[food] != library[item_to_make]
       error_counter += -1
@@ -29,12 +20,9 @@ def serving_size_calc(item_to_make, num_of_ingredients)
     raise ArgumentError.new("#{item_to_make} is not a valid input")
   end
 
-#Raises error when input is not cake, cookie, or pie
-
-#defines serving_size as the value of whatever key of hash 'library' is called
   serving_size = library.values_at(item_to_make)[0]
-  remaining_ingredients = num_of_ingredients - serving_size
-#defines remaining_ingredients as the value of the called key stores in array, and modulus serving_size to get the remainder.
+  remaining_ingredients = num_of_ingredients % serving_size
+
   case remaining_ingredients
   when 0
     return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}"
@@ -42,7 +30,6 @@ def serving_size_calc(item_to_make, num_of_ingredients)
     return "Calculations complete: Make #{num_of_ingredients / serving_size} of #{item_to_make}, you have #{remaining_ingredients} leftover ingredients. Suggested baking items: TODO: MAKE THIS FEATURE"
   end
 end
-#Takes remaining_ingredients, if they equal zero returns a statement of what item to make and how many to make.  If there are remaining ingredients it has an incomplete suggested baking items feature.
 
 p serving_size_calc("pie", 7)
 p serving_size_calc("pie", 8)
@@ -51,3 +38,5 @@ p serving_size_calc("cake", 7)
 p serving_size_calc("cookie", 1)
 p serving_size_calc("cookie", 10)
 p serving_size_calc("THIS IS AN ERROR", 5)
+
+#  Reflection
